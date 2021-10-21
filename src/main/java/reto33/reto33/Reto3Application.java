8d4b81de6e7054d01c566210b2e53a5edde4c0ba
guti <hernan.gutierrez1.mt@correo.usa.edu.co>
2021-10-20 21:52:35 -0500

    primera maquina

---
+++ src/main/java/reto33/reto33/Reto3Application.java
@@ -0,0 +1,18 @@
+
+package reto33.reto33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author hguti
+ */
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
